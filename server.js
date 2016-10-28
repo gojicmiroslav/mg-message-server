64a807c3154cd417da22293be41bcbc27fe3a489
gojicmiroslav <miroslavy2k@gmail.com>
2016-10-28 20:21:03 +0200

    Add body parser and CORS support

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 var port = process.env.PORT || 8080;
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+// CORS Support
+app.use(function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+  	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  	next();
+});
 
 app.get('/', function(req, res) {
     res.send('<h1>Hello, It is working!!!</h1>');
